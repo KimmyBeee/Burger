8989ff6d43db890114c0fc1f0fd4d6a2d224d463
kimmybeee <kimariehavertybozman@gmail.com>
2018-03-01 22:09:41 -0800

    db set up and seeded

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) 
+VALUES 
+	("The Clove and Hoof Burger"), 
+	("The Double Duke Park Burger"), 
+	("The Absinthe SF Burger"),
+	("5 Guys Double Cheeseburger with Extra Pickes");
+
